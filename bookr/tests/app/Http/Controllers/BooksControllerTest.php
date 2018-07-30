2ede32cae9ca9f98e5489cac98565b5a11b79e5f
Paisit Jarunnamsiri <paisit.j@gmail.com>
2018-07-30 23:46:29 +0800

    Use Model Factories and a Test Database for Tests

--- bookr/tests/app/Http/Controllers/BooksControllerTest.php
+++ bookr/tests/app/Http/Controllers/BooksControllerTest.php
@@ -9,6 +9,8 @@ use TestCase;
 
 class BooksControllerTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /** @test **/
     public function index_status_code_should_be_200()
     {
@@ -18,27 +20,28 @@ class BooksControllerTest extends TestCase
     /** @test **/
     public function index_should_return_a_collection_of_records()
     {
-        $this
-            ->get('/books')
-            ->seeJson([
-                'title' => 'War of the Worlds'
-            ])
-            ->seeJson([
-                'title' => 'A Wrinkle in Time'
-            ]);
+        $books = factory('App\Book', 2)->create();
+
+        $this->get('/books');
+
+        foreach ($books as $book) {
+            $this->seeJson(['title' => $book->title]);
+        }
     }
 
     /** @test **/
     public function show_should_return_a_valid_book()
     {
+        $book = factory('App\Book')->create();
+
         $this
-            ->get('/books/1')
+            ->get("/books/{$book->id}")
             ->seeStatusCode(200)
             ->seeJson([
-                'id' => 1,
-                'title' => 'War of the Worlds',
-                'description' => 'A science fiction masterpiece about Martians invading London',
-                'author' => 'H. G. Wells'
+                'id' => $book->id,
+                'title' => $book->title,
+                'description' => $book->description,
+                'author' => $book->author
             ]);
 
         $data = json_decode($this->response->getContent(), true);
@@ -101,11 +104,13 @@ class BooksControllerTest extends TestCase
     /** @test **/
     public function update_should_only_change_fillable_fields()
     {
-        $this->notSeeInDatabase('books', [
-            'title' => 'The War of the Worlds'
+        $book = factory('App\Book')->create([
+            'title' => 'War of the Worlds',
+            'description' => 'A science fiction masterpiece about Martians invading London',
+            'author' => 'H. G. Wells',
         ]);
         
-        $this->put('/books/1', [
+        $this->put("/books/{$book->id}", [
             'id' => 5,
             'title' => 'The War of the Worlds',
             'description' => 'The book is way better than the movie.',
@@ -114,7 +119,7 @@ class BooksControllerTest extends TestCase
 
         $this->seeStatusCode(200)
             ->seeJson([
-                'id' => 1,
+                'id' => $book->id,
                 'title' => 'The War of the Worlds',
                 'description' => 'The book is way better than the movie.',
                 'author' => 'Wells, H. G.'
@@ -147,12 +152,14 @@ class BooksControllerTest extends TestCase
     /** @test **/
     public function destroy_should_remove_a_valid_book()
     {
+        $book = factory('App\Book')->create();
+
         $this
-            ->delete('/books/1')
+            ->delete("/books/{$book->id}")
             ->seeStatusCode(204)
             ->isEmpty();
 
-        $this->notSeeInDatabase('books', ['id' => 1]);
+        $this->notSeeInDatabase('books', ['id' => $book->id]);
     }
 
     /** @test **/
