d2d641d53d0c8be11d3559bf082134a2b1e688ef
Paisit Jarunnamsiri <paisit.j@gmail.com>
2018-07-30 23:18:56 +0800

    Delete a Book

--- bookr/app/Http/Controllers/BooksController.php
+++ bookr/app/Http/Controllers/BooksController.php
@@ -83,4 +83,25 @@ class BooksController extends Controller
 
         return $book;
     }
+
+    /**
+     * DELETE /books/{id}
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+        $book->delete();
+
+        return response(null, 204);
+    }
 }
